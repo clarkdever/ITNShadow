b0c60af62cc8ec5fd88671900f0ed051ee418157
Clark Dever <clarkdever@gmail.com>
2014-11-22 19:12:08 -0500

    Moved website to root directory

---
+++ js/routes.js
@@ -0,0 +1,30 @@
+"use strict";
+
+angular.module('myApp.routes', ['ngRoute'])
+
+ .config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/info', {
+        templateUrl:  'partials/info.html'
+      }).
+      when('/home', {
+        templateUrl:  'partials/home.html'
+      }).
+      when('/students', {
+        templateUrl:  'partials/students.html'
+      }).
+      when('/companies', {
+        templateUrl:  'partials/companies.html'
+      }).
+      when('/contact', {
+        templateUrl:  'partials/contact.html'
+      }).
+      when('/register', {
+        templateUrl:  'partials/register.html'
+      }).
+      otherwise({
+        redirectTo: 'partials/home.html'
+      });
+  }]);
+
